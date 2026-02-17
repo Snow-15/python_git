0088a2f57f217a4588284318e558d664487fb683
Snow-15 <github.dimple756@passinbox.com>
2026-02-17 10:30:46 +0800

    Finished part 3

--- libwyag.py
+++ libwyag.py
@@ -10,6 +10,37 @@ import re
 import sys
 import zlib # For compressing 
 
+
+class GitRepository(object):
+    """A git repository"""
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+        
+        # Before creating a repo object it checks first if there is a .git dir
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception(f"Not a Git repository {path}")
+
+        # Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception(f"Unsupported repositoryformatversion: {vers}")
+
+
 argparser = argparse.ArgumentParser(description="The stupidest content tracker") 
 
 # To handle subcommands in git: init, add, status etc.
@@ -18,6 +49,13 @@ argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 
 # We're telling it that git must have a subcommand like: git COMMAND and not just git
 argsubparsers.required = True
+
+argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository")
+argsp.add_argument("path",
+                   metavar="directory",
+                   nargs="?",
+                   default=".",
+                   help="Where to create the repository")
 
 def main(argv=sys.argv[1:]): # We're removing the filename inside of the sys.argv list
     args = argparser.parse_args(argv)
@@ -40,5 +78,85 @@ def main(argv=sys.argv[1:]): # We're removing the filename inside of the sys.arg
         case "tag"          : cmd_tag(args)
         case _              : print("Bad command.")
 
+
+def repo_path(repo, *path):
+    """Returns the combined .git directory and *paths path"""
+    return os.path.join(repo.gitdir, *path)
+
+
+def repo_file(repo, *path, mkdir=False):
+    """
+    Same as repo_path, but create dirname(*path) if absent. For
+    example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will
+    create .git/refs/remotes/origin
+    """
+
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path, but mkdir *path if absent if mkdir"""
+
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception(f"Not a directory {path}")
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+def repo_create(path):
+    """Create a new repository at path"""
+
+    repo = GitRepository(path, True)
+
+    # Checks if the path doesn't exist or an empty directory
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception(f"{path} is not a directory!")
+        if os.path.exists(repo.gitdir) and os.listdir(repo.gitdir):
+            raise Exception(f"{path} is not empty")
+    else:
+        os.makedirs(repo.worktree)
+
+    # Creates the other subdirectories inside of .git
+    assert repo_dir(repo, "branches", mkdir=True)
+    assert repo_dir(repo, "objects", mkdir=True)
+    assert repo_dir(repo, "refs", "tags", mkdir=True)
+    assert repo_dir(repo, "refs", "heads", mkdir=True)
+
+    # creates the .git/description file
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("refs: refs/heads/master\n")
+        
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+
+    return ret
+
+def cmd_init(args):
+    repo_create(args.path)
 
 
